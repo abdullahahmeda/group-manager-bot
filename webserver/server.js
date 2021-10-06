1557653139b0398d6fe11f99b7baa77567b0f5cc
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2021-10-06 17:30:23 +0200

    add login screen

--- webserver/server.js
+++ webserver/server.js
@@ -2,12 +2,20 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const path = require('path')
 const jsonfile = require('jsonfile')
+const session = require('express-session')
 const { paths } = require('../constants')
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+app.use(session({
+  secret: 'h6[pSJQ>"mgWx&7j',
+  resave: false,
+  saveUninitialized: true
+}))
 
 nunjucks.configure(path.resolve(__dirname, 'views'), {
   autoescape: true,
@@ -16,9 +24,22 @@ nunjucks.configure(path.resolve(__dirname, 'views'), {
 })
 
 app.get('/', async (req, res) => {
+  if (!req.session.isLoggedIn) {
+    return res.render('login.html')
+  }
   const automaticReplies = await jsonfile.readFile(paths.automaticReplies)
   res.render('index.html', {
     automaticReplies
+  })
+})
+
+app.post('/', (req, res) => {
+  if (req.body.email === 'Nomani1993@gmail.com' && req.body.password === 'Aa112233') {
+    req.session.isLoggedIn = true
+    return req.session.save((_) => res.redirect('/'))
+  }
+  res.render('login.html', {
+    error: 'الإيميل أو كلمة السر غير صحيحين'
   })
 })
 
