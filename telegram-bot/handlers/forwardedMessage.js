36107bdd9ccd04f4bd911fe391c007c36720f3f6
Abdullah Ahmed <abdullah.ahmed.a2000@gmail.com>
2021-10-23 22:58:14 +0200

    Fix for forwarded messages

--- telegram-bot/handlers/forwardedMessage.js
+++ telegram-bot/handlers/forwardedMessage.js
@@ -4,7 +4,7 @@ const db = require('../../db')
 function handleForwardedMessage (msg) {
   const chatId = msg.chat.id
   const messageId = msg.message_id
-  const forwardId = msg?.forward_from?.id || msg?.forward_from_chat?.id
+  const forwardId = msg?.from?.id || msg?.forward_from_chat?.id
 
   const stmt = db.prepare('SELECT * FROM allowed_forwards WHERE username = ?')
   const user = stmt.get(`${forwardId}`)
